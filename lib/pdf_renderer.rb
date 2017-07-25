159ffeccdb79061327a5a75c75cb957b039bb4e9
Cristiano Alencar <cristiano.alencar@integragrp.com.br>
2017-07-25 06:50:31 -0300

    Creating pdf renderer

--- lib/pdf_renderer.rb
+++ lib/pdf_renderer.rb
@@ -1,3 +1,22 @@
-module PdfRenderer
-  # Your code goes here...
+## Defining our code inside module is not
+## needed since we are using the render api.
+## 
+## Thus, since we're not defining methods
+## and/or classes, there is no way of
+## getting conflicts.
+##
+## That's why the following (automacally
+## generated) code isn't needed.
+# module PdfRenderer
+#   # Your code goes here...
+# end
+
+require "prawn"
+
+# using render api
+ActionController::Renderers.add :pdf do |filename, options|
+  pdf = Prawn::Document.new
+  pdf.text render_to_string(options)
+  send_data(pdf.render, filename: "#{filename}.pdf",
+      disposition: "attachment") # sends pdf as attachment
 end
